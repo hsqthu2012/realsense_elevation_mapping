edc65b509611d8e212f7cd7cb75a839baf122307
hsqthu2012 <hsq_thu2012@163.com>
2021-09-15 20:54:24 +0800

    use OpenMP to acclerate visualMap loop

--- GEM/elevation_mapping/elevation_mapping/src/ElevationMap.cpp
+++ GEM/elevation_mapping/elevation_mapping/src/ElevationMap.cpp
@@ -32,6 +32,9 @@
 // Eigen
 #include <Eigen/Dense>
 
+// OpenMP
+#include <omp.h> // added by Suqin He
+
 using namespace std;
 using namespace grid_map;
 
@@ -73,6 +76,76 @@ void ElevationMap::setGeometry(const grid_map::Length& length, const double& res
 }
 
 
+// sensor_msgs::ImagePtr ElevationMap::show(ros::Time timeStamp, string robot_name, float trackPointTransformed_x, float trackPointTransformed_y, int length, float *elevation, float *var, int *point_colorR, int *point_colorG, int *point_colorB, float *rough, float *slope, float *traver, float* intensity)
+// {
+//   cv::Mat image(length, length, CV_8UC3, cv::Scalar(0,0,0));
+
+//   visualMap_.clearAll();
+
+//   pcl::PointCloud<pcl::PointXYZRGB> show_pointCloud;
+//   pcl::PointXYZRGB show_point;
+
+//   int index, index_x, index_y;
+//   Index start_index = visualMap_.getStartIndex();
+
+//   // ros::Time begin_time = ros::Time::now (); // added by Suqin He
+  
+//   for (GridMapIterator iterator(visualMap_); !iterator.isPastEnd(); ++iterator) {
+//     index_x = (*iterator).transpose().x();
+//     index_y = (*iterator).transpose().y();
+//     index = index_x * length + index_y;
+//     // ROS_INFO("elevation[index] = %f", elevation[index]); // added by Suqin He for debugging
+//     if(elevation[index] != -10)
+//     {
+//       visualMap_.at("elevation", *iterator) = elevation[index];
+//       // visualMap_.at("variance", *iterator) = var[index];
+//       // visualMap_.at("rough", *iterator) = rough[index];
+//       // visualMap_.at("slope", *iterator) = slope[index];
+//       // visualMap_.at("traver", *iterator) = traver[index];
+//       // visualMap_.at("color_r", *iterator) = point_colorR[index];
+//       // visualMap_.at("color_g", *iterator) = point_colorG[index];
+//       // visualMap_.at("color_b", *iterator) = point_colorB[index];
+//       // visualMap_.at("intensity", *iterator) = intensity[index];
+
+//       // Position point;
+//       // visualMap_.getPosition(*iterator, point);
+
+//       // show_point.x = point.x();
+//       // show_point.y = point.y();
+//       // show_point.z = visualMap_.at("elevation", *iterator);
+//       // show_point.r = visualMap_.at("color_r", *iterator);
+//       // show_point.g = visualMap_.at("color_g", *iterator);
+//       // show_point.b = visualMap_.at("color_b", *iterator);
+
+//       // show_pointCloud.push_back(show_point);
+//       // image.at<cv::Vec3b>((index_x + length - start_index[0]) % length, (index_y + length - start_index[1]) % length)[0] = visualMap_.at("color_b", *iterator);
+//       // image.at<cv::Vec3b>((index_x + length - start_index[0]) % length, (index_y + length - start_index[1]) % length)[1] = visualMap_.at("color_g", *iterator);
+//       // image.at<cv::Vec3b>((index_x + length - start_index[0]) % length, (index_y + length - start_index[1]) % length)[2] = visualMap_.at("color_r", *iterator);
+//     }
+//   }
+//   // ROS_INFO("Loop time: %f ms.", (ros::Time::now() - begin_time).toSec()*1000); // added by Suqin He
+
+//   // Publish orthomoasic image
+//   sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", image).toImageMsg();
+//   orthomosaicPublisher_.publish(msg);
+
+//   pcl_conversions::toPCL(ros::Time::now(), show_pointCloud.header.stamp);
+//   show_pointCloud.header.frame_id = "/" + robot_name + "/map";
+
+//   if(show_pointCloud.size() > 0)
+//   {
+//       sensor_msgs::PointCloud2 pub_pointcloud;
+//       pcl::toROSMsg(show_pointCloud, pub_pointcloud);
+//       VpointsPublisher_.publish(pub_pointcloud); 
+//   }
+
+//   grid_map_msgs::GridMap message;
+//   GridMapRosConverter::toMessage(visualMap_, message);
+//   visualMapPublisher_.publish(message);
+//   return msg;
+// }
+
+// rewrite this function using OpenMP for better efficiency. Suqin He
 sensor_msgs::ImagePtr ElevationMap::show(ros::Time timeStamp, string robot_name, float trackPointTransformed_x, float trackPointTransformed_y, int length, float *elevation, float *var, int *point_colorR, int *point_colorG, int *point_colorB, float *rough, float *slope, float *traver, float* intensity)
 {
   cv::Mat image(length, length, CV_8UC3, cv::Scalar(0,0,0));
@@ -82,19 +155,19 @@ sensor_msgs::ImagePtr ElevationMap::show(ros::Time timeStamp, string robot_name,
   pcl::PointCloud<pcl::PointXYZRGB> show_pointCloud;
   pcl::PointXYZRGB show_point;
 
-  int index, index_x, index_y;
   Index start_index = visualMap_.getStartIndex();
 
   // ros::Time begin_time = ros::Time::now (); // added by Suqin He
   
-  for (GridMapIterator iterator(visualMap_); !iterator.isPastEnd(); ++iterator) {
-    index_x = (*iterator).transpose().x();
-    index_y = (*iterator).transpose().y();
-    index = index_x * length + index_y;
-    // ROS_INFO("elevation[index] = %f", elevation[index]); // added by Suqin He for debugging
+  uint mapSize = visualMap_.getSize().prod();
+
+  #pragma omp parallel for
+  for (uint i = 0; i < mapSize; ++i) {
+    const Index mapIndex(getIndexFromLinearIndex(i, visualMap_.getSize()));
+    uint index = mapIndex(0) * length + mapIndex(1);
     if(elevation[index] != -10)
     {
-      visualMap_.at("elevation", *iterator) = elevation[index];
+      visualMap_.at("elevation", mapIndex) = elevation[index];
       // visualMap_.at("variance", *iterator) = var[index];
       // visualMap_.at("rough", *iterator) = rough[index];
       // visualMap_.at("slope", *iterator) = slope[index];
